cd2438dd9ce7887aa2518cbe93f2de1a971d8ec1
yourisev <sevgama44@gmail.com>
2023-05-23 21:19:08 -0400

    Experimented environment property configuration and injection.

--- src/main/java/com/training/peam/dependencyinjection_demo/config/PropertyConfig.java
+++ src/main/java/com/training/peam/dependencyinjection_demo/config/PropertyConfig.java
@@ -1,15 +1,20 @@
 package com.training.peam.dependencyinjection_demo.config;
 
 import com.training.peam.dependencyinjection_demo.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+    @Autowired
+    Environment env;
 
     @Value("${guru.username}")
     String user;
@@ -23,7 +28,7 @@ public class PropertyConfig {
      @Bean
     public FakeDataSource fakeDataSource(){
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(user);
+        fakeDataSource.setUser(env.getProperty("PEAM_USERNAME"));
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
