9609526a5f14b3fc99f2e7d6ef4d990726738dbb
yourisev <sevgama44@gmail.com>
2023-05-23 20:50:32 -0400

    Created a property Java based config and configured our main method such that the FakeDataSource receives data from the property file

--- src/main/java/com/training/peam/dependencyinjection_demo/DependencyInjectionDemoApplication.java
+++ src/main/java/com/training/peam/dependencyinjection_demo/DependencyInjectionDemoApplication.java
@@ -4,6 +4,7 @@ import com.training.peam.dependencyinjection_demo.controllers.Constructorinjecte
 import com.training.peam.dependencyinjection_demo.controllers.DemoController;
 import com.training.peam.dependencyinjection_demo.controllers.PropertyinjectedController;
 import com.training.peam.dependencyinjection_demo.controllers.SetterinjectedController;
+import com.training.peam.dependencyinjection_demo.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -16,6 +17,8 @@ public class DependencyInjectionDemoApplication {
         ApplicationContext context = SpringApplication.run(DependencyInjectionDemoApplication.class, args);
         DemoController controller = (DemoController) context.getBean("demoController");
 
+        FakeDataSource fakeDataSource = (FakeDataSource) context.getBean(FakeDataSource.class);
+        System.out.println(fakeDataSource.getUser());
 
     }
 
